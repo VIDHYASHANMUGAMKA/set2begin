1dbbe29883143653a062a607d3d741e3f85ea6ab
VIDHYASHANMUGAMKA <37236109+VIDHYASHANMUGAMKA@users.noreply.github.com>
2019-02-05 15:03:21 +0530

    Update and rename multiplies.py to arithmetic addition.py

---
+++ arithmetic addition.py
@@ -0,0 +1,7 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+num3 = input('Enter third number: ')
+sum = float(num1) + float(num2)+float(num3)
+
+
+print('The sum of {0} ,{1} and {2} is {3}'.format(num1, num2, num3, sum)) 
